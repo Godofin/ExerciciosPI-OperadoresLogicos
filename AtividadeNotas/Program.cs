6d8452f85647366d64d9e72bc857f2f4cd18afc9
Guilherme Henrique Lavezzo <guilherme.lavezzo@programmers.com.br>
2022-06-06 10:41:31 -0300

    Upando código github

---
+++ AtividadeNotas/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Adicione a média do aluno");
+double media = double.Parse(Console.ReadLine());
+
+if (media < 5)
+    Console.WriteLine("O aluno foi reprovado.");
+else if (media > 6 || media < 7)
+    Console.WriteLine("O aluno está em recuperação");
+else
+    Console.WriteLine("O aluno foi aprovado");
+
+//Utilizando ternário
+
+var resultadoAluno = (media >= 8 ? "Aprovado" : "Reprovado");
+
+Console.WriteLine($"A situação do aluno foi que o aluno foi: {resultadoAluno}");
---
+++ ValidacaoIdade/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Console.WriteLine("Digite a sua idade: ");
+int idade1 = int.Parse(Console.ReadLine());
+
+if (idade1 >= 18)
+    Console.WriteLine("Pode tirar carteira");
+else
+    Console.WriteLine("Não pode tirar carteira");
+*/
+
+/*Console.WriteLine("Digite sua idade: ");
+for (int idade = int.Parse(Console.ReadLine()); idade < 18; idade++){
+    Console.WriteLine($"Idade: {idade}, Não pode tirar carta");
+}
+
+Console.WriteLine("Agora você pode tirar sua carta, já tem mais de 18 anos");
+*/
+
+//Operadores Ternários
+//Sintaxe: testeLogico ? valor se verdadeiro : valor false
+
+int a = 10;
+int b = 40;
+
+var resultado = (a == b ? "Sim": "Não");
+
+Console.WriteLine(resultado);
